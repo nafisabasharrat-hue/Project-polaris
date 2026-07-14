894df65df95eefdacb4485aa1174aede7bd430ae
nafisabasharrat-hue <nafisabasharrat@gmail.com>
2026-07-14 21:12:49 +0600

    Create memories array with multiple memory entries
    Added an array of memory objects with titles, images, and text descriptions.

--- data.js
+++ data.js
@@ -1,1 +1,93 @@
+const memories = [
 
+{
+    title:"Night 🌙",
+    image:"assets/photos/memory1.jpg",
+    text:"Your first sweet goodnight message that still makes me blush."
+},
+
+{
+    title:"Memory 2",
+    image:"assets/photos/memory2.jpg",
+    text:"Write your memory here."
+},
+
+{
+    title:"Memory 3",
+    image:"assets/photos/memory3.jpg",
+    text:"Write your memory here."
+},
+
+{
+    title:"Memory 4",
+    image:"assets/photos/memory4.jpg",
+    text:"Write your memory here."
+},
+
+{
+    title:"Memory 5",
+    image:"assets/photos/memory5.jpg",
+    text:"Write your memory here."
+},
+
+{
+    title:"Memory 6",
+    image:"assets/photos/memory6.jpg",
+    text:"Write your memory here."
+},
+
+{
+    title:"Memory 7",
+    image:"assets/photos/memory7.jpg",
+    text:"Write your memory here."
+},
+
+{
+    title:"Memory 8",
+    image:"assets/photos/memory8.jpg",
+    text:"Write your memory here."
+},
+
+{
+    title:"Memory 9",
+    image:"assets/photos/memory9.jpg",
+    text:"Write your memory here."
+},
+
+{
+    title:"Memory 10",
+    image:"assets/photos/memory10.jpg",
+    text:"Write your memory here."
+},
+
+{
+    title:"Memory 11",
+    image:"assets/photos/memory11.jpg",
+    text:"Write your memory here."
+},
+
+{
+    title:"Memory 12",
+    image:"assets/photos/memory12.jpg",
+    text:"Write your memory here."
+},
+
+{
+    title:"Memory 13",
+    image:"assets/photos/memory13.jpg",
+    text:"Write your memory here."
+},
+
+{
+    title:"Memory 14",
+    image:"assets/photos/memory14.jpg",
+    text:"Write your memory here."
+},
+
+{
+    title:"Memory 15",
+    image:"assets/photos/memory15.jpg",
+    text:"Write your memory here."
+}
+
+];
