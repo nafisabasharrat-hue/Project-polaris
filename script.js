637f377c812603734358dcff987e6c38b873cf2d
nafisabasharrat-hue <nafisabasharrat@gmail.com>
2026-07-23 01:53:51 +0600

    Add random animation duration to shooting stars

--- script.js
+++ script.js
@@ -196,6 +196,9 @@ function createShootingStar(){
 
     star.className = "shooting-star";
 
+    star.style.animationDuration =
+    (1.8 + Math.random() * 1.8) + "s";
+
     star.style.top = Math.random() * 35 + "%";
     star.style.left = "100%";
 
