89bf32dc2943b41ea031229139b4d8b46751967d
nafisabasharrat-hue <nafisabasharrat@gmail.com>
2026-08-01 02:34:54 +0600

    Update script.js

--- script.js
+++ script.js
@@ -130,12 +130,24 @@ for(let i=0;i<30;i++){
     starContainer.appendChild(star);
 
 }
-
 enterButton.addEventListener("click", () => {
 
     document.getElementById("intro-screen").style.display = "none";
 
     document.getElementById("universe").classList.remove("hidden");
+
+    music.volume = 0;
+    music.currentTime = 0;
+
+    setTimeout(() => {
+
+        music.play().catch(console.error);
+
+        fadeMusic();
+
+    },2000);
+
+});
 
 });
 
